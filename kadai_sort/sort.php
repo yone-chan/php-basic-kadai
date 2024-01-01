4486f411b3a9e78cee8b338c49165601da3ec9d0
yone-chan <142673364+yone-chan@users.noreply.github.com>
2024-01-01 12:02:02 +0900

    Add files via upload

---
+++ kadai_sort/sort.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
+    <p>
+        <?php
+       
+        function sort_2way ($array, $order) {
+          if ($order == true) {
+            echo '昇順にソートします。<br>';
+            sort($array);
+            } else {
+            echo '降順にソートします。<br>';
+            rsort($array);
+          }
+          foreach ($array as $value) {
+            echo $value . '<br>';
+          }
+        }
+
+        $array = [15, 4, 18, 23, 10];
+
+        sort_2way($array, true);
+
+        sort_2way($array, false);
+        ?>
+    </p>
+</body>
+
+</html>
