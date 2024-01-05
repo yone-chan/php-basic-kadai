7f05af55d5f4e230f2a65e89b6e0b96c45fb6203
yone-chan <142673364+yone-chan@users.noreply.github.com>
2024-01-06 03:04:01 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+  <head>
+    <meta charset="utf-8">
+    <title>PHP基礎編</title>
+  </head>
+
+  <body>
+    <p>
+      <?php
+      $veggies = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+      print_r($veggies);
+      ?>
+    <p>
+  </body>
+
+</html>
